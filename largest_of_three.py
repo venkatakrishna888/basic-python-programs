6913adca4409d4b14e60d7abf3fe1e38f01bab2e
VENKATAKRISHNA BATTULA <venkatakrisha42@gmail.com>
2026-03-08 11:48:09 +0530

    Create largest_of_three.py
    added succsesfully

---
+++ largest_of_three.py
@@ -0,0 +1,7 @@
+a = int(input("ENTER FIRST NUMBER: "))
+b = int(input("ENTER SECOND NUMBER: "))
+c = int(input("ENTER THIRD NUMBER: "))
+
+largest = max(a, b, c)
+
+print("Largest number:", largest)
